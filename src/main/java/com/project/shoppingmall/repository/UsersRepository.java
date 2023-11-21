e42b2aaca71207be41eefa331f72acdea4c649b7
ksubin <ksubin1017@gmail.com>
2023-11-21 22:36:26 +0900

    #8 - product 테이블 및 관련 기능 작성
    product 엔티티를 작성하고 엔티티에 직접적인 setter  사용을 피하고 데이터를 전송할 `ProductDTO`를 작성했다.
    또한 `ProductRepository`를 통해 카테고리만 불러올 수 있게 기능을 구현했다.

--- src/main/java/com/project/shoppingmall/repository/UsersRepository.java
+++ src/main/java/com/project/shoppingmall/repository/UsersRepository.java
@@ -2,7 +2,9 @@ package com.project.shoppingmall.repository;
 
 import com.project.shoppingmall.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsersRepository extends JpaRepository<Users, String> {
 
 }
