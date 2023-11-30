04ad8d33f7f9981d95d8e9c972a2f19f082fad80
ksubin <ksubin1017@gmail.com>
2023-11-30 22:48:24 +0900

    #12 - 유저 클래스 수정
    추후 어드민 서비스에서 Spring Security를 통해 역할을 부여하기 위해 유저 클래스 컬럼을 추가했다.

---
+++ src/main/java/com/project/shoppingmall/dto/UserDTO.java
@@ -0,0 +1,39 @@
+package com.project.shoppingmall.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    @NotBlank
+    @Length(min = 6, max = 16, message = "아이디는 6글자 이상 16글자 이하여야합니다.")
+    @Pattern(regexp = "^[a-z0-9]+$", message = "아이디는 영소문자와 숫자만 가능합니다")
+    public String userId;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    public String pwd;
+
+    @NotBlank
+    public String name;
+
+    @NotBlank
+    public String address;
+
+    @NotBlank
+    @Pattern(regexp="[0-9]+$", message="숫자만 입력 가능합니다.")
+    public String phone;
+
+    @NotBlank
+    @Pattern(regexp = "^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\\.[A-Za-z0-9\\-]+$", message = "이메일 형식이 올바르지 않습니다.")
+    public String email;
+
+    public String birth;
+
+}
