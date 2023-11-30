5d1fccfe6ae0b583aa880b7a86c7d9c1c2f8b5c9
ksubin <ksubin1017@gmail.com>
2023-11-30 22:54:58 +0900

    #12 - 예외 처리
    회원가입에서 사용자가 유효성 검증을 통과하지 못했을 때 발생하는 400에러를 처리하기 위해 추가했다.

---
+++ src/main/java/com/project/shoppingmall/exception/ErrorController.java
@@ -0,0 +1,17 @@
+package com.project.shoppingmall.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e) {
+        return "/400";
+    }
+}
