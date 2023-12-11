c600938c2564b303867d5db72f76ce9a26919bd9
ksubin <ksubin1017@gmail.com>
2023-12-11 21:33:24 +0900

    #18 - review 도메인
    review 엔티티와 DTO, repository 구현

---
+++ src/main/java/com/project/shoppingmall/repository/ReviewRepository.java
@@ -0,0 +1,15 @@
+package com.project.shoppingmall.repository;
+
+import com.project.shoppingmall.domain.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, String> {
+
+    Page<Review> findByProductId(Long productId, Pageable pageable);
+}
