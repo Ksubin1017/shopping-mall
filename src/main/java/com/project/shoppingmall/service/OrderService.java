1628fe5b59a727255649d6576ef1c7280b50dd59
ksubin <ksubin1017@gmail.com>
2023-12-11 21:35:25 +0900

    #18 - review 구매자
    구매자를 확인하기 위한 repository와 기능을 구현한 service 생성

--- src/main/java/com/project/shoppingmall/service/OrderService.java
+++ src/main/java/com/project/shoppingmall/service/OrderService.java
@@ -5,6 +5,8 @@ import com.project.shoppingmall.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class OrderService {
 
@@ -17,5 +19,9 @@ public class OrderService {
 
     public void saveOrder(Orders orders) {
         orderRepository.save(orders);
+    }
+
+    public List<Orders> findOrder(String userId) {
+       return orderRepository.findByUserId(userId);
     }
 }
