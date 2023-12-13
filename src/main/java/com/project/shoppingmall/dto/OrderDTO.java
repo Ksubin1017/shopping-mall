7879c5cd43a61051dbed4dfa4e9223430aa2afbe
ksubin <ksubin1017@gmail.com>
2023-12-13 21:51:59 +0900

    #20 - admin 주문 내역 조회 및 승인 처리
    admin이 주문 내역을 조회하고 주문 요청을 승인 처리할 수 있도록 update를 구현했다.

--- src/main/java/com/project/shoppingmall/dto/OrderDTO.java
+++ src/main/java/com/project/shoppingmall/dto/OrderDTO.java
@@ -7,15 +7,6 @@ import lombok.Setter;
 @Setter
 public class OrderDTO {
 
-    public String orderNum;
+    public String orderStatus;
 
-    public String userId;
-
-    public String address;
-
-    public Long productId;
-
-    public String productName;
-
-    public String productPrice;
 }
--- src/main/java/com/project/shoppingmall/service/OrderService.java
+++ src/main/java/com/project/shoppingmall/service/OrderService.java
@@ -1,9 +1,13 @@
 package com.project.shoppingmall.service;
 
 import com.project.shoppingmall.domain.Orders;
+import com.project.shoppingmall.dto.OrderDTO;
 import com.project.shoppingmall.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,6 +26,16 @@ public class OrderService {
     }
 
     public List<Orders> findOrder(String userId) {
-       return orderRepository.findByUserId(userId);
+       return orderRepository.findByUserIdOrderByOrderCreatedAtDesc(userId);
+    }
+
+    public Page<Orders> orderStatus(String orderStatus, Pageable pageable) {
+        return orderRepository.findByOrderStatus(orderStatus, pageable);
+    }
+
+    @Transactional
+    public void updateOrderStatus(String orderNum, OrderDTO orderDTO) {
+        Orders orders = orderRepository.findByOrderNum(orderNum);
+        orders.updateOrderStatus(orderDTO.getOrderStatus());
     }
 }
