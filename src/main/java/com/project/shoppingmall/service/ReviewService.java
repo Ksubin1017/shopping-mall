2dffaadfbb05aac7483c97b4697ccc7ce1f00da6
ksubin <ksubin1017@gmail.com>
2023-12-13 21:51:07 +0900

    #20 - review 조회 및 삭제
    사용자가 작성한 리뷰를 조회 및 삭제할 수 있도록 DB의 데이터를 가져오는 로직을 구성했다.

--- src/main/java/com/project/shoppingmall/service/ReviewService.java
+++ src/main/java/com/project/shoppingmall/service/ReviewService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -26,5 +27,14 @@ public class ReviewService {
 
     public Page<Review> findReview(Long productId, Pageable pageable) {
         return reviewRepository.findByProductId(productId, pageable);
+    }
+
+    public List<Review> findReviewUserId(String userId) {
+        return reviewRepository.findByUserIdOrderByReviewCreatedAtDesc(userId);
+    }
+
+    @Transactional
+    public void reviewDel(String reviewNum) {
+        reviewRepository.deleteByReviewNum(reviewNum);
     }
 }
