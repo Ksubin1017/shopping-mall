aa48b73398e636c340d58684c3a2606a1e9d0208
ksubin <ksubin1017@gmail.com>
2023-12-14 21:45:32 +0900

    #22 - 메인 뷰 BEST&NEW
    메인 뷰에 BEST와 NEW를 전달하기 위한 controller이다.

--- src/main/java/com/project/shoppingmall/controller/MainController.java
+++ src/main/java/com/project/shoppingmall/controller/MainController.java
@@ -1,20 +1,36 @@
 package com.project.shoppingmall.controller;
 
+import com.project.shoppingmall.domain.Product;
+import com.project.shoppingmall.service.ProductService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class MainController {
+
+    private final ProductService productService;
+
+    public MainController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/")
-    public String main(HttpSession session) {
+    public String main(HttpSession session, Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String userId = authentication.getName();
-
         session.setAttribute("userId", userId);
+
+        List<Product> bestProduct = productService.bestProduct();
+        List<Product> newProduct = productService.newProduct();
+
+        model.addAttribute("bestProduct", bestProduct);
+        model.addAttribute("newProduct", newProduct);
         return "/main";
     }
 }
